ab59e45724a5c2c5c2e81a1fb588b8d52dede9bc
Sayak Bhattacharya <sayak6237@gmail.com>
2022-11-12 23:05:20 +0530

    Fixed carousel slider

--- components/dash_slider/dash_slider.js
+++ components/dash_slider/dash_slider.js
@@ -6,8 +6,8 @@ import styles from './dash_slider.module.scss'
 const responsive = {
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
-      items: 4,
-      slidesToSlide: 4 // optional, default to 1.
+      items: 3,
+      slidesToSlide: 3 // optional, default to 1.
     },
     tablet: {
       breakpoint: { max: 1024, min: 464 },
@@ -24,14 +24,18 @@ const responsive = {
 const DashSlider = ({data}) => {
     return ( 
         <div className={styles.slider}>
+
+        <h1>Popular movies</h1>
+
         <div className={styles.cards}>
-        <Carousel swipeable={false} responsive={responsive}  draggable={false}
-        >
-        {data.results.slice(0,7).map((item, index) => (
-                    <GlassCard key={index} details={item} ht={17.6} 
-                        wd={24} hf={1.2} sf={0.8} rf={0.9}/>
-                    ))}
-        </Carousel>
+            <Carousel swipeable={true} responsive={responsive}  draggable={false}
+            className={styles.itemClass}
+            >
+            {data.results.map((item, index) => (
+                        <GlassCard key={index} details={item} ht={13.9} 
+                            wd={19} hf={1} sf={0.6} rf={0.9}/>
+                        ))}
+            </Carousel>
         </div>
 
         </div>
--- components/favourite/favouritePage.js
+++ components/favourite/favouritePage.js
@@ -26,7 +26,7 @@ const FavouriteComponent = ({nowPlaying}) => {
         <Carousel swipeable={false} responsive={responsive}  draggable={false}
         className={styles.itemClass}
         >
-        {nowPlaying.results.slice(0,7).map((item, index) => (
+        {nowPlaying.results.slice(0,10).map((item, index) => (
                     <GlassCard key={index} details={item} ht={17.6} 
                         wd={24} hf={1.2} sf={0.8} rf={0.9}/>
                     ))}
